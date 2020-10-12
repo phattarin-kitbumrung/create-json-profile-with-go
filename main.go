77fe708f4413ccd5b362e78968c4d573072a5a58
phattarin kitbumrung <pm.phobia@gmail.com>
2020-10-12 12:34:20 +0700

    modified some struct

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 
 
 type Minizymint struct {
-	Minizymint []minizymint `json:"minizymint"`
+	Profile []Profile `json:"minizymint"`
 }
 
-type minizymint struct {
+type Profile struct {
 	Name   string `json:"name"`
 	Nickname   string `json:"nickname"`
 	Birthday string `json:"birthday"`
@@ -51,50 +51,40 @@ type WorkedExperiences struct {
 
 func main() {
 	fmt.Println("***** Welcome to Phattarin Kitbumrung Profile *****")
-
-	// Open our jsonFile
 	jsonFile, err := os.Open("profile.json")
-	// if we os.Open returns an error then handle it
 	if err != nil {
 		fmt.Println(err)
 	}
 
-
-	//fmt.Println("Successfully Opened profile.json")
-	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
-
-
-	// read our opened xmlFile as a byte array.
 	byteValue, _ := ioutil.ReadAll(jsonFile)
-
 	var minizymint Minizymint
 	json.Unmarshal(byteValue, &minizymint)
 
-	for i := 0; i < len(minizymint.Minizymint); i++ {
-		fmt.Println("Name: " + minizymint.Minizymint[i].Name)
-		fmt.Println("Nickname: " + minizymint.Minizymint[i].Nickname)
-		fmt.Println("Birthday: " + minizymint.Minizymint[i].Birthday)
-		fmt.Println("Religion: " + minizymint.Minizymint[i].Religion)
-		fmt.Println("Address: " + minizymint.Minizymint[i].Address)
-		fmt.Println("Email: " + minizymint.Minizymint[i].Email)
-		fmt.Println("Mobile: " + minizymint.Minizymint[i].Mobile)
-		fmt.Println("Github: " + minizymint.Minizymint[i].Github)
+	for i := 0; i < len(minizymint.Profile); i++ {
+		fmt.Println("Name: " + minizymint.Profile[i].Name)
+		fmt.Println("Nickname: " + minizymint.Profile[i].Nickname)
+		fmt.Println("Birthday: " + minizymint.Profile[i].Birthday)
+		fmt.Println("Religion: " + minizymint.Profile[i].Religion)
+		fmt.Println("Address: " + minizymint.Profile[i].Address)
+		fmt.Println("Email: " + minizymint.Profile[i].Email)
+		fmt.Println("Mobile: " + minizymint.Profile[i].Mobile)
+		fmt.Println("Github: " + minizymint.Profile[i].Github)
 		fmt.Println("Education >>>")
-		fmt.Println("Bachelor Degree: " + minizymint.Minizymint[i].Education.BachelorDegree)
-		fmt.Println("Master Degree: " + minizymint.Minizymint[i].Education.MasterDegree)
-		fmt.Println("University: " + minizymint.Minizymint[i].Education.University)
+		fmt.Println("Bachelor Degree: " + minizymint.Profile[i].Education.BachelorDegree)
+		fmt.Println("Master Degree: " + minizymint.Profile[i].Education.MasterDegree)
+		fmt.Println("University: " + minizymint.Profile[i].Education.University)
 		fmt.Println("Skills >>>")
-		fmt.Println("Operating Systems: " + minizymint.Minizymint[i].Skills.OperatingSystems)
-		fmt.Println("Programming Language: " + minizymint.Minizymint[i].Skills.ProgrammingLanguage)
-		fmt.Println("Database: " + minizymint.Minizymint[i].Skills.Database)
-		fmt.Println("Tools: " + minizymint.Minizymint[i].Skills.Tools)
-		fmt.Println("Knowledge & Technology: " + minizymint.Minizymint[i].Knowledge)
-		fmt.Println("Interested: " + minizymint.Minizymint[i].Interested)
+		fmt.Println("Operating Systems: " + minizymint.Profile[i].Skills.OperatingSystems)
+		fmt.Println("Programming Language: " + minizymint.Profile[i].Skills.ProgrammingLanguage)
+		fmt.Println("Database: " + minizymint.Profile[i].Skills.Database)
+		fmt.Println("Tools: " + minizymint.Profile[i].Skills.Tools)
+		fmt.Println("Knowledge & Technology: " + minizymint.Profile[i].Knowledge)
+		fmt.Println("Interested: " + minizymint.Profile[i].Interested)
 		fmt.Println("Worked Experiences >>>")
-		fmt.Println("Cooperative Education: " + minizymint.Minizymint[i].WorkedExperiences.CooperativeEducation)
-		fmt.Println("Web Developer: " + minizymint.Minizymint[i].WorkedExperiences.WebDeveloper)
-		fmt.Println("Backend Developer: " + minizymint.Minizymint[i].WorkedExperiences.BackendDeveloper)
-		fmt.Println("Programmer: " + minizymint.Minizymint[i].WorkedExperiences.Programmer)
+		fmt.Println("Cooperative Education: " + minizymint.Profile[i].WorkedExperiences.CooperativeEducation)
+		fmt.Println("Web Developer: " + minizymint.Profile[i].WorkedExperiences.WebDeveloper)
+		fmt.Println("Backend Developer: " + minizymint.Profile[i].WorkedExperiences.BackendDeveloper)
+		fmt.Println("Programmer: " + minizymint.Profile[i].WorkedExperiences.Programmer)
 	}
 }
